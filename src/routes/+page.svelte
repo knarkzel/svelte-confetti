c951194eb40d3bfdd828ce33f01336e3b4cc56e8
Knarkzel <knarkzel@gmail.com>
2023-07-10 20:51:49 +0200

    Latest

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { tick } from "svelte";
+  import { Confetti } from "svelte-confetti";
+
+  let active = false;
+  async function showConfetti() {
+    active = false;
+    await tick();
+    active = true;
+  }
+</script>
+
+<h1>Hello there</h1>
+<button on:click={showConfetti}>Show some confetti</button>
+
+{#if active}
+  <Confetti infinite amount={200} />
+{/if}
